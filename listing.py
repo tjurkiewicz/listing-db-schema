0978cdf5a41e8b5f9828c22cd8ce71f85345c2de
Tomasz Jurkiewicz <tomasz.jurkiewicz@polidea.com>
2017-08-04 22:05:01 +0200

    Fixed model column type

--- listing.py
+++ listing.py
@@ -7,7 +7,7 @@ Base = sqlalchemy.ext.declarative.declarative_base()
 class Listing(Base):
     __tablename__ = 'listing'
 
-    id = sqlalchemy.Column(sqlalchemy.Sequence, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     listing_id = sqlalchemy.Column(sqlalchemy.Integer)
     last_update = sqlalchemy.Column(sqlalchemy.TIMESTAMP(timezone=True))
     city = sqlalchemy.Column(sqlalchemy.String(length=64))
